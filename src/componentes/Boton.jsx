931e224cb2a957b83bfe48cd6a2e03b4920960a6
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-15 00:17:01 -0500

    Proyecto calculadora finalizada

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -7,7 +7,7 @@ function Boton (props){
     }
 
     return(
-        <div className={ `boton-contenedor ${esOperador(props.children) ? "operador" : null}`.trimEnd() }>
+        <div className={ `boton-contenedor ${esOperador(props.children) ? "operador" : null}`.trimEnd() } onClick={ ()=> props.manejarClic(props.children)}>
             {props.children}
         </div>
     );
